3d877b1b5d372ef33c00621afe549d5a7be1b03a
Mikhail Gorokhov <mikhail.gorokhov@vicuesoft.com>
2019-11-22 20:24:25 +0700

    Add GetFileSize

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 #include <QFile>
 
 #include <windows.h>
+#include <io.h>
+
+static const QString FILE_PATH = "./example.txt";
+static const QString text = "poop";
+
+void writeText(const QString &text);
 
 int main(int argc, char *argv[])
 {
@@ -10,16 +16,36 @@ int main(int argc, char *argv[])
 
 #ifdef _WIN32
     QFile qFile("./example.txt");
-    qFile.open(QIODevice::WriteOnly);
 
-//    HANDLE hFile;
-//    auto dwSize = GetFileSize (hFile, nullptr);
-//    if (dwSize == INVALID_FILE_SIZE)
-//    {
-//        auto dwError = GetLastError() ;
-//        qWarning() << QString("Error %1").arg(dwError);
-//    }
+    writeText(text);
+    qFile.open(QIODevice::ReadOnly);
+
+    auto fd = qFile.handle();    // C descriptor
+    HANDLE hFile = reinterpret_cast<HANDLE>(_get_osfhandle(fd));
+
+    auto dwSize = GetFileSize (hFile, nullptr);
+    if (dwSize == INVALID_FILE_SIZE)
+    {
+        auto dwError = GetLastError() ;
+        qWarning() << QString("Error %1").arg(dwError);
+    }
+    else
+    {
+        qInfo() << QString("Success, size is %1").arg(dwSize);
+    }
 #endif
 
     return a.exec();
+}
+
+void writeText(const QString &text) {
+    QFile qFile(FILE_PATH);
+    qFile.open(QIODevice::WriteOnly);
+
+    uint64_t textLength = static_cast<uint64_t>(text.length());
+    char *data = new char[textLength];
+    strncpy(data, qPrintable(text), textLength);
+
+    qFile.write(data, text.length());
+    qFile.close();
 }
